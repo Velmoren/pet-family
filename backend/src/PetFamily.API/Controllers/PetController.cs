03dc671cbf1804f12d07ba1184e816b2120c5acb
ilyasovas <ilyasovas@fsa.gov.ru>
2026-07-02 16:32:25 +0300

    B-3 & B-4

---
+++ backend/src/PetFamily.API/Controllers/PetController.cs
@@ -0,0 +1,39 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Mvc;
+using PetFamily.Domain.Volunteer;
+
+namespace PetFamily.API.Controllers;
+
+public class PetController : Controller
+{
+    [HttpGet]
+    public IActionResult Get(string name, string description)
+    {
+        Result<Pet> petResult = Pet.Create(name, description);
+
+        if (petResult.IsFailure)
+        {
+            return BadRequest(petResult.Error);
+        }
+        
+        var result = Save(petResult.Value);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(petResult.Error);
+        }
+
+        return Ok();
+    }
+    
+    public Result Save(Pet pet)
+    {
+        if (pet != null)
+        {
+            return Result.Success();
+        }
+
+        return Result.Failure("error");
+    }
+}
+
---
+++ backend/src/PetFamily.Domain/Volunteer/PetPhoto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace PetFamily.Domain.Volunteer;
+
+public class PetPhoto
+{
+    public string StoragePath { get; set; } = string.Empty;
+    
+    public bool IsMain { get; set; }
+}
